8ecda5dd52233460839b7a814954b815c5cbca99
Antonio Mori <camargomoritrancoso@hotmail.com>
2023-10-19 17:02:22 -0300

    fixing "encontre" & "maior " functions

--- src/main/App.java
+++ src/main/App.java
@@ -1,14 +1,46 @@
 package main;
 public class App {
+
     public static int encontre(int[] arrayOrdenado, int num){
-        return -1;
+        
+        int resposta = 0;
+       
+        for(int i = 0; i < arrayOrdenado.length; i++){
+            if(arrayOrdenado[i] == num){
+                resposta =1;
+            
+            }
+        }
+        return resposta;
     }
 
+
+    //função que divide o array em dois
+
+
     public static int maior(int[] arrayDesordenado){
-        return -1;
+                
+        int maior = arrayDesordenado[0];
+        
+        for(int i = 0; i < arrayDesordenado.length; i++){
+
+            if(arrayDesordenado[i] > maior){
+                maior = arrayDesordenado[i];
+            }
+        }
+        return maior;
     }
+
 
     public static Integer parMaisProximo(Integer[][] pares){
         return 1;
     }
+
+
+
+
+
+
+
+
 }
