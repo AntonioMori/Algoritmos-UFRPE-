9d127fcd0aebe3e73e5dab57eb6d88a1aa7cbf0b
Antonio Mori <camargomoritrancoso@hotmail.com>
2023-10-19 17:16:05 -0300

    fixing last function

--- src/main/App.java
+++ src/main/App.java
@@ -3,9 +3,12 @@ public class App {
 
     public static int encontre(int[] arrayOrdenado, int num){
         
-        int resposta = 0;
+        int resposta = 0; // caso base se não encontrar
        
+        // começa em 0, vai até o tamanho do array, e incrementa de 1 em 1
         for(int i = 0; i < arrayOrdenado.length; i++){
+            
+            //se depois de passar por todo arrey achar o num igual vai modificar a resp
             if(arrayOrdenado[i] == num){
                 resposta =1;
             
@@ -15,25 +18,39 @@ public class App {
     }
 
 
-    //função que divide o array em dois
-
-
     public static int maior(int[] arrayDesordenado){
-                
+        
+    //começa com o primeiro elemento do array sendo o maior
         int maior = arrayDesordenado[0];
         
+    //durante todo o array vai se comparar o maior atual com o num referente ao indice
+    //verificado. Se o num for maior que o maior atual, o maior atual passa a ser o num
         for(int i = 0; i < arrayDesordenado.length; i++){
 
             if(arrayDesordenado[i] > maior){
                 maior = arrayDesordenado[i];
             }
         }
-        return maior;
+
+        return maior; //retorna o resultado
     }
 
 
     public static Integer parMaisProximo(Integer[][] pares){
-        return 1;
+        
+        //mesma lógica do maior, mas agora com 2 dimensões
+        int menorPar = pares[0][0];
+        
+        // durante todo o array vai verificar se o primeiro elemento do par é 
+        //menor que o menorPar
+        for(int i = 0; i < pares.length; i++){
+            if(pares[i][0] < menorPar){
+                menorPar = pares[i][0];
+            }
+        }
+
+        
+        return menorPar;
     }
 
 
